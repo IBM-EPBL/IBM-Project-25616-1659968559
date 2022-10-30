a2318947b901118e45c873dae63629aae0e60845
venkateshaPrabhu811 <kingvenkat521@gmail.com>
2022-10-30 16:31:10 +0530

    Assignment 1 Updated

--- Assessments/Team Lead(Venkatesha Prabhu)/Assignment 1/index.js
+++ Assessments/Team Lead(Venkatesha Prabhu)/Assignment 1/index.js
@@ -2,6 +2,31 @@ var x = document.getElementById('login');
 var y = document.getElementById('register');
 var z = document.getElementById('btn');
 
+var registerUser;
+var loginUser;
+function userReg(){
+    let username = document.querySelector('#regName').value
+    registerUser = Array.from(document.querySelectorAll('#register input'))
+    .reduce((acc,input) => ({
+        ...acc,[input.placeholder] : input.value
+    }),{});
+    localStorage.setItem("User_Details",username);
+    locationChange();
+}
+
+function userLog(){
+    let username = document.querySelector('#logName').value
+    loginUser = Array.from(document.querySelectorAll('#login input'))
+    .reduce((acc,input) => ({
+        ...acc,[input.placeholder] : input.value
+    }),{});
+    localStorage.setItem("User_Details",username);
+    locationChange();
+}
+
+function locationChange(){
+    window.location = "greet.html";
+}
 
 function register(){
     x.style.left = "-400px";
